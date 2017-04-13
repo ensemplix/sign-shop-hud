df811d3de650fe90ad7ef9768a7146b91e3252ad
ensirius <ensirius@gmail.com>
2017-04-13 11:55:33 +0300

    Отрефакторена отрисовка всплывающего окна

--- src/main/java/ru/ensemplix/gui/SignShopHudGui.java
+++ src/main/java/ru/ensemplix/gui/SignShopHudGui.java
@@ -1,54 +1,77 @@
 package ru.ensemplix.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.item.ItemStack;
-import org.lwjgl.opengl.GL11;
 import ru.ensemplix.SignShop;
 
+import static org.lwjgl.opengl.GL11.*;
+
+// Данный класс занимается отрисовкой всплывающего окна.
 public class SignShopHudGui extends GuiScreen {
 
     private static final Minecraft minecraft = Minecraft.getMinecraft();
+    private static final TextureManager textureManager = minecraft.getTextureManager();
+    private static final FontRenderer fontRenderer = minecraft.fontRendererObj;
+
+    private static final int BACKGROUND_TOP_PADDING = 60;
+    private static final int BACKGROUND_WIDTH = 340;
+    private static final int BACKGROUND_HEIGHT = 300;
+
+    private final int width;
+    private final int scale;
+    private final int centerX;
 
     public SignShopHudGui(SignShop shop) {
         ScaledResolution scaled = new ScaledResolution(minecraft, minecraft.displayWidth, minecraft.displayHeight);
-        int width = scaled.getScaledWidth();
-        int height = scaled.getScaledHeight();
-        int factor = scaled.getScaleFactor();
-        int centerX = width / 2;
+        width = scaled.getScaledWidth();
+        scale = scaled.getScaleFactor();
+        centerX = width / 2;
 
-        int backgroundXL = centerX - 170 / factor;
-        int backgroundXR = centerX + 170 / factor;
-        int backgroundYT = 60 / factor;
-        int backgroundYB = backgroundYT + 300 / factor;
+        drawBackground();
+        drawItem(shop.getStack());
+    }
 
-        drawGradientRect(backgroundXL, backgroundYT, backgroundXR, backgroundYB, 0xC0101010, 0xD0101010);
-        drawHorizontalLine(backgroundXL, backgroundXR - 1, backgroundYT, 0xFFFFFFFF);
-        drawHorizontalLine(backgroundXL, backgroundXR - 1, backgroundYB - 1, 0xFFFFFFFF);
-        drawVerticalLine(backgroundXL, backgroundYT, backgroundYB, 0xFFFFFFFF);
-        drawVerticalLine(backgroundXR - 1, backgroundYT, backgroundYB, 0xFFFFFFFF);
+    private void drawBackground() {
+        int backgroundXLeft = centerX - BACKGROUND_WIDTH / 2 / scale;
+        int backgroundXRight = centerX + BACKGROUND_WIDTH / 2 / scale;
+        int backgroundYTop = BACKGROUND_TOP_PADDING / scale;
+        int backgroundYBottom = backgroundYTop + BACKGROUND_HEIGHT / scale;
 
-        int scale = 2;
-        int itemSize = 15;
-        TextureManager textureManager = minecraft.getTextureManager();
-        ItemStack stack = shop.getStack();
+        drawGradientRect(backgroundXLeft, backgroundYTop, backgroundXRight, backgroundYBottom, 0xC0101010, 0xD0101010);
+        drawHorizontalLine(backgroundXLeft, backgroundXRight - 1, backgroundYTop, 0xFFFFFFFF);
+        drawHorizontalLine(backgroundXLeft, backgroundXRight - 1, backgroundYBottom - 1, 0xFFFFFFFF);
+        drawVerticalLine(backgroundXLeft, backgroundYTop, backgroundYBottom, 0xFFFFFFFF);
+        drawVerticalLine(backgroundXRight - 1, backgroundYTop, backgroundYBottom, 0xFFFFFFFF);
+    }
 
-        if(factor == 1) {
-            scale = 4;
-            itemSize = 30;
+    private void drawItem(ItemStack stack) {
+        int itemScale = 0;
+        int itemSize = 0;
+
+        switch(scale) {
+            case 1:
+                itemScale = 4;
+                itemSize = 30;
+                break;
+            default:
+                itemScale = 2;
+                itemSize = 15;
+                break;
         }
 
-        GL11.glPushMatrix();
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glScalef(scale, scale, 1.0f);
-        itemRender.renderItemAndEffectIntoGUI(minecraft.fontRenderer, textureManager, stack, (centerX - itemSize) / scale, 75 / factor / scale);
-        itemRender.renderItemIntoGUI(minecraft.fontRenderer, textureManager, stack, (centerX - itemSize) / scale, 75 / factor / scale);
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        GL11.glPopMatrix();
+        glPushMatrix();
+        glEnable(GL_LIGHTING);
+        glEnable(GL_DEPTH_TEST);
+        glScalef(itemScale, itemScale, 1.0F);
+        itemRender.renderItemAndEffectIntoGUI(fontRenderer, textureManager, stack, (centerX - itemSize) / itemScale, 75 / scale / itemScale);
+        itemRender.renderItemIntoGUI(fontRenderer, textureManager, stack, (centerX - itemSize) / itemScale, 75 / scale / itemScale);
+        glDisable(GL_LIGHTING);
+        glDisable(GL_DEPTH_TEST);
+        glPopMatrix();
     }
 
 }
